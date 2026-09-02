67d39770d75cf82201d0f95b46a3643607668c54
dgnt7k <tuanna@edmicro.vn>
2026-09-02 15:16:07 +0700

    done

--- src/app/features/events/event-card.ts
+++ src/app/features/events/event-card.ts
@@ -1,19 +1,25 @@
 import { DatePipe, NgOptimizedImage } from '@angular/common';
 import { Component, computed, input, linkedSignal, output } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { UiCard } from '../../shared/ui-card';
+import { ClickLogger } from '../../shared/directives/click-logger';
 
 @Component({
   selector: 'app-event-card',
-  imports: [DatePipe, RouterLink, NgOptimizedImage],
+  imports: [DatePipe, RouterLink, NgOptimizedImage, UiCard],
+  hostDirectives: [
+    {
+      directive: ClickLogger,
+      inputs: ['eventName: trackingId'],
+    },
+  ],
   template: `
-    <div
-      class="bg-white rounded-lg shadow-md overflow-hidden hover:shadow-xl transition-shadow duration-300"
-    >
+    <app-ui-card>
       <!-- TODO: Add Image -->
-      <div class="relative h-48 w-full bg-gray-200">
+      <div card-header class="relative h-48 w-full bg-gray-200">
         <img
           [ngSrc]="image()"
-          width="500"
+          width="360"
           height="200"
           priority
           class="object-cover w-full h-full max-h-full max-w-full"
@@ -65,17 +71,16 @@ import { RouterLink } from '@angular/router';
             Remove
           </button>
         </div>
-
-        <div class="mt-4 pt-4 border-t border-gray-100 text-right">
-          <a
-            [routerLink]="['/event', id()]"
-            class="text-blue-600 font-medium hover:underline cursor-pointer"
-          >
-            View Details →
-          </a>
-        </div>
       </div>
-    </div>
+      <div card-footer class="mt-4 text-right">
+        <a
+          [routerLink]="['/event', id()]"
+          class="text-blue-600 font-medium hover:underline cursor-pointer"
+        >
+          View Details →
+        </a>
+      </div>
+    </app-ui-card>
   `,
 })
 export class EventCard {
---
+++ src/app/features/events/venue-map.ts
@@ -0,0 +1,18 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
+
+@Component({
+  selector: 'app-venue-map',
+  imports: [NgOptimizedImage],
+  template: `
+    <div class="h-140 bg-gray-200 rounded mb-4 overflow-hidden relative">
+      <img
+        [ngSrc]="'/images/venue-map.png'"
+        width="500"
+        height="600"
+        class="w-full h-full object-cover"
+      />
+    </div>
+  `,
+})
+export class VenueMap {}
